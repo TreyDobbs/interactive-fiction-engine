09d5a21beda6e63ece102a1d65ed9c7fd7f0218e
Trey Dobbs <58719746+TreyDobbs@users.noreply.github.com>
2022-03-11 18:12:33 -0600

    Update ull.h

--- ull.h
+++ ull.h
@@ -1,8 +1,5 @@
 // --------------------------------------------------------------------------
 // Name: Trey Dobbs
-// Course-Section: CS 255-01
-// Assignment: Project #6
-// Date Due: 11/20/2020
 // Description: This C++ project adds support for in-game items to our 
 // interactive fiction engine. Item support has been added to Area and
 // Player, the map constructor has been updated, five new commands including 
